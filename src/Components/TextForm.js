5ae1c77f5a1dbda1dcaea484c2e2a84cf36c32a7
Vicky <coder.vc1@gmail.com>
2023-04-15 00:43:37 +0530

    Putting actions as in the Dropdown menu

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -43,33 +43,55 @@ export default function TextForm(props) {
    // setText("new Text") // Correct way to change the state
 
   return (
-    <>
-    <div className='container'>
-        <div className="mb-3">
+    <>   
+    
+     <div className="container">
+      <table className='table my-3 mx-2' border="1px" width="100%" bgcolor='gray' font-style="Times New Roman">
+
+         <tr>
             
-            <h1>{props.heading}</h1>
+            <td>
+                <div className='container'>
+                    <div className="mb-3">
+                        
+                        <h1>{props.heading}</h1>
+                        
+                        <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Enter text here" id="myBox" rows="8"></textarea>
+                    </div>
+
+                </div>
+            </td>
             
-            <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Enter text here" id="myBox" rows="8"></textarea>
+            <td>
+                    <div className="container my-3">
+                        <h2>Your text summary</h2>
+                        <p><b>{text.split(" ").length} Words and {text.length} characters</b> </p>
+                        <p>{0.008 * text.split("").length} Minutes read</p>
+
+                        {/* <h2>Preview</h2>
+                        <p>{text}</p> */}
+                    </div>
+             </td>
+
+            </tr>
+        </table>
+
+       
+        <div className="dropdown mx-3">
+            <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Actions
+            </button>
+            <ul className="dropdown-menu">
+
+                <li><button className='btn btn-success my-1' onClick={handleUpClick}>Convert to UpperCase</button></li>
+                <li><button className='btn btn-info my-1' onClick={handleLowClick}>Convert to LowerCase</button></li>
+                <li><button className="btn btn-primary my-1 " onClick={handleExtraSpaces}>Remove Extra Spaces</button></li>
+                <li><button className='btn btn-warning my-1' onClick={handleClearClick}>Clear</button></li>
+            </ul>
         </div>
 
-        <button className='btn btn-success' onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className='btn btn-info mx-2' onClick={handleLowClick}>Convert to LowerCase</button>
-        <button className='btn btn-warning mx-2' onClick={handleClearClick}>Clear</button>
-        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
-
-    <div className="container my-3">
-        <h2>Your text summary</h2>
-        <p><b>{text.split(" ").length} Words and {text.length} characters</b> </p>
-        <p>{0.008 * text.split("").length} Minutes read</p>
-
-        <h2>Preview</h2>
-        <p>{text}</p>
-    </div>
-    
-
-
-
+           
     </>
   )
 }
